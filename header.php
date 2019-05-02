3e59880a363d1d7cdf80b7a6bb3569955e212e0d
Eduardo Cardinal <edcrdnl@gmail.com>
2019-05-02 10:00:27 +0800

    Update header.php

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 
@@ -24,39 +23,39 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'edcrdnl' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding page-width">
-			<?php
-			if ( has_custom_logo() ) :
-				the_custom_logo();
-			else :
-				$edcrdnl_description = get_bloginfo( 'description', 'display' );
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php echo $edcrdnl_description; /* WPCS: xss ok. */ ?></p>
-					<?php
+	<header id="masthead" class="site-header alt">
+		<div class="row page-width">
+			<div class="site-branding span-1-4 vertical-align-middle">
+				<?php
+				if ( has_custom_logo() ) :
+					the_custom_logo();
 				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<p class="site-description"><?php echo $edcrdnl_description; /* WPCS: xss ok. */ ?></p>
-					<?php
+					$edcrdnl_description = get_bloginfo( 'description', 'display' );
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<p class="site-description"><?php echo $edcrdnl_description; /* WPCS: xss ok. */ ?></p>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<p class="site-description"><?php echo $edcrdnl_description; /* WPCS: xss ok. */ ?></p>
+						<?php
+					endif;
 				endif;
-			endif;
-			?>
-		</div><!-- .site-branding -->
+				?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<div class="page-width">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'edcrdnl' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-			</div>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation span-3-4 vertical-align-middle">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="fas fa-bars"></span> <?php esc_html_e( 'Menu', 'edcrdnl' ); ?></button>
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div><!-- .row -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
